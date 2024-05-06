9f342ca13e3628b00f5085023ef5cb5e440414d9
yunji28 <parkyunji24@sookmyung.ac.kr>
2024-05-07 01:42:38 +0900

    1_Lab04

---
+++ 1_Lab04_2.c
@@ -0,0 +1,21 @@
+//27433
+#include <stdio.h>
+long long factorial(int n)
+{
+	if ((n == 1) || (n==0))
+	{
+		return 1;
+	}
+	else
+	{
+		return  (n*factorial(n - 1));
+	}
+}
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	printf("%lld\n", factorial(N));
+	return 0;
+}
